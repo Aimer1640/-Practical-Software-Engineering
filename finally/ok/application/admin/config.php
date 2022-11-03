1671f6fb38f2c48fbd2fdfb5cb3eb936f6c36b79
Davie <2959176602@qq.com>
2022-11-03 23:52:45 +0800

    教师端

--- finally/ok/application/admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-return [
-    'view_replace_str'=>[
-        '__ADMIN__'=>'/ok/public/static/admin',
-        '__editor__'=>'/ok/public/static/ueditor'
-    ]
-];
--- finally/ok/application/index/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-return[
-
-    'view_replace_str'=>[
-        '__INDEX__'=>'/ok/public/static/index',
-        '__editor__'=>'/ok/public/static/ueditor'
-    ]
-];
